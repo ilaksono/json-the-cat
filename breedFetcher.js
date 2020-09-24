09c62138f304a48abfff8157b3e65f25f0a43b6d
ilaksono <ian.laksono@gmail.com>
2020-09-24 13:55:43 +0000

    added async test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,40 @@
 const request = require('request');
 
-const search = process.argv[2];
+// const search = process.argv[2];
+const catUrl = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-const fetcher = str => {
-  request(str, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
+// const fetcher = str => {
+//   request(catUrl + search, (error, response, body) => {
+//     console.log('error:', error); // Print the error if one occurred
+//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//     // console.log('body:', body,); // Print the HTML for the Google homepage.
+//     if (!error) {
+//       if (!body) {
+//         console.log('Request failed, exiting...');
+//         return process.exit(12);
+//       } else {
+//         let obj = JSON.parse(body)[0];
+//         // console.log(typeof obj);
+//         // let key;
+//         if (obj) {
+//           // key = Object.keys(obj);
+//           // console.log(key[0]);
+//           console.log('Description: ', obj.description);
+//           // console.log('Object: ', obj);
+//         } else {
+//           console.log(` ${search} was not found in the cat search api`);
+//         }
+//       }
+//     } else {
+//       console.log(`${str} is Invalid URL, Exiting...`);
+//       return process.exit(-1);
+//     }
+//   });
+// };
+
+// fetcher(search);
+const fetchBreedDescription = (str, cb) => {
+  request(catUrl + str, (error, response, body) => {
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     // console.log('body:', body,); // Print the HTML for the Google homepage.
     if (!error) {
@@ -18,10 +48,12 @@ const fetcher = str => {
         if (obj) {
           // key = Object.keys(obj);
           // console.log(key[0]);
-          console.log('Description: ', obj.description);
+          // console.log('Description: ', obj.description);
+          cb(error, obj.description);
           // console.log('Object: ', obj);
         } else {
-          console.log(` ${search} was not found in the cat search api`);
+          console.log('error:', error); // Print the error if one occurred
+          console.log(` ${str} was not found in the cat search api`);
         }
       }
     } else {
@@ -31,7 +63,7 @@ const fetcher = str => {
   });
 };
 
-fetcher('https://api.thecatapi.com/v1/breeds/search?q=' + search);
-
 
 //https://api.thecatapi.com/v1/breeds/search?q=siberian
+
+module.exports = { fetchBreedDescription };
